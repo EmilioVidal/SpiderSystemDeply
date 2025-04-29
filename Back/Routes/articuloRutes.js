e55e4204e91e994355cba483a1a79a245d3e84a7
tonogutierrez <antoniogtzmireles@gmail.com>
2025-04-28 23:58:48 -0600

    Endpoints para articulos

--- Back/Routes/articuloRutes.js
+++ Back/Routes/articuloRutes.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { getArticulos } = require('../Controllers/articulos');
+const { getArticulos,getTotalArticulos,getTotalProductos } = require('../Controllers/articulos');
 
 // Ruta para obtener artículos
 router.get('/getarticulos', getArticulos);
+//Ruta para obtener el total 
+router.get('/getTotalArticulos',getTotalArticulos);
+//Ruta para contar los estados
+router.get('/gettotalproductos', getTotalProductos);
+
+
 
 module.exports = router;
