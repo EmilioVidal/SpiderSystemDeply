5b8f8dd5bdf2ae93b294282ddd243cb679af6006
EmilioVidal <149820545+EmilioVidal@users.noreply.github.com>
2025-05-01 13:35:36 -0600

    conexión del front con vercel junto a Railway

--- Back/index.js
+++ Back/index.js
@@ -3,25 +3,22 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+const FRONTEND_URL = process.env.FRONTEND_URL || 'spidersystemdeply-production.up.railway.app';
+
+app.use(cors({ origin: FRONTEND_URL }));
+app.use(express.json());
+
 const loginRoutes = require('./Routes/loginRutas');
 const articuloRoutes = require('./Routes/articuloRutes');
 const { connectToHANA } = require('./Config/confDB');
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-
 
 // Rutas de la API
 app.use('/api/login', loginRoutes);
 app.use('/api', articuloRoutes);
 
-
-
-
-//El servidor link
+// El servidor link
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, async () => {
-    await connectToHANA();
-    console.log(`Backend corriendo en http://localhost:${PORT}`);
+  await connectToHANA();
+  console.log(`Backend corriendo en http://localhost:${PORT}`);
 });
